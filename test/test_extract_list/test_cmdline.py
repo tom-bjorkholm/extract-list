5838abd0490b6d457febd4019bd264b5424e6d66
Tom Bjorkholm <t@tombjorkholm.se>
2024-12-25 17:41:38 +0200

    test OK command line parsing

--- test/test_extract_list/test_cmdline.py
+++ test/test_extract_list/test_cmdline.py
@@ -119,7 +119,11 @@ def test_cmdline_ok2(capsys,  # pylint: disable=too-many-positional-arguments,to
                            {'oval': 'mickey', 'ival': 'duck',
                             'cval': 'donald'}),
                           ('extract -c KK -o MM -i LL',
-                           {'oval': 'MM', 'ival': 'LL', 'cval': 'KK'})])
+                           {'oval': 'MM', 'ival': 'LL', 'cval': 'KK'}),
+                          ('python -m extract_list.py extract ' +
+                           '-i abc.xml -o def.xlsx -c ghi.cfg',
+                           {'cval': 'ghi.cfg', 'ival': 'abc.xml',
+                            'oval': 'def.xlsx'})])
 def test_cmdline_ok3(capsys, monkeypatch, line: str, vals):
     """Test parsing of command line for extract."""
     def patch(in_file_name: str, cfg_file_name: str,
@@ -141,7 +145,11 @@ def test_cmdline_ok3(capsys, monkeypatch, line: str, vals):
 @pytest.mark.parametrize('line, vals',
                          [('cfg-example -k example_json -t txt -o GH.cfg',
                            {'kval': 'example_json', 'tval': 'txt',
-                            'oval': 'GH.cfg'})])
+                            'oval': 'GH.cfg'}),
+                          ('python3 -m extract_list/__main.py ' +
+                           'cfg-example -o abc.cfg -t csv -k example_xml',
+                           {'kval': 'example_xml', 'tval': 'csv',
+                            'oval': 'abc.cfg'})])
 def test_cmdline_ok4(capsys, monkeypatch, line: str, vals):
     """Test parsing of command line for cfg-example."""
     def patch(filename: str, cfgtype: str,
@@ -159,3 +167,71 @@ def test_cmdline_ok4(capsys, monkeypatch, line: str, vals):
     _ = extract_cmd(arguments=cmd)
     assert patch.calls == 1
     check_capsys(capsys=capsys)
+
+
+@pytest.mark.parametrize('line,errmsgs',
+                         [('-o out.xlsx -c cfg.cfg -i in.json',
+                           ['error: argument subparser_name: invalid choice',
+                            'usage: extract_list [-h] {cfg-example,extract}',
+                            "(choose from 'cfg-example', 'extract')"]),
+                          ('extract -i in.jspon -o out.xlsx -c a.cfg -b',
+                           ['extract_list: error: unrecognized ' +
+                            'arguments: -b']),
+                          ('extract -i in.jspon -o out.xlsx',
+                           ['error: the following arguments are required: ' +
+                            '-c/--cfg',
+                            'usage: extract_list extract [-h] -c CFG ' +
+                            '-i INPUT -o OUTPUT']),
+                          ('extract -o out.xlsx -c c.cfg',
+                           ['error: the following arguments are required: ' +
+                            '-i/--input',
+                            'usage: extract_list extract [-h] -c CFG ' +
+                            '-i INPUT -o OUTPUT']),
+                          ('cfg-example',
+                           ['error: the following arguments are required: ' +
+                            '-k/--kind, -t/--typeofoutput, -o/--output']),
+                          ('cfg-example -k abc -t csv -o out.cfg',
+                           ['usage: extract_list cfg-example [-h] -k',
+                            '{sw_json_to_rrs,sw_xml_to_rrs,' +
+                            'example_json,example_xml}',
+                            '-t {excel,csv,json,xml,txt} -o OUTPUT',
+                            "invalid choice: 'abc' (choose from " +
+                            "'sw_json_to_rrs', 'sw_xml_to_rrs', " +
+                            "'example_json', 'example_xml')"]),
+                          ('cfg-example -t csv -o out.cfg',
+                           ['usage: extract_list cfg-example [-h] -k',
+                            '{sw_json_to_rrs,sw_xml_to_rrs,' +
+                            'example_json,example_xml}',
+                            '-t {excel,csv,json,xml,txt} -o OUTPUT',
+                            'extract_list cfg-example: error: the ' +
+                            'following arguments are required: -k/--kind']),
+                          ('cfg-example -k sw_json_to_rrs -o out.cfg',
+                           ['usage: extract_list cfg-example [-h] -k',
+                            '{sw_json_to_rrs,sw_xml_to_rrs,' +
+                            'example_json,example_xml}',
+                            '-t {excel,csv,json,xml,txt} -o OUTPUT',
+                            'extract_list cfg-example: error: the ' +
+                            'following arguments are required: ' +
+                            '-t/--typeofoutput']),
+                          ('cfg-example -k sw_json_to_rrs -t abc -o out.cfg',
+                           ['usage: extract_list cfg-example [-h] -k',
+                            '{sw_json_to_rrs,sw_xml_to_rrs,' +
+                            'example_json,example_xml}',
+                            '-t {excel,csv,json,xml,txt} -o OUTPUT',
+                            "extract_list cfg-example: error: argument " +
+                            "-t/--typeofoutput: invalid choice: 'abc' " +
+                            "(choose from 'excel', 'csv', 'json', 'xml', " +
+                            "'txt')"]),
+                          ('cfg-example -k sw_json_to_rrs -t csv',
+                           ['usage: extract_list cfg-example [-h] -k',
+                            '{sw_json_to_rrs,sw_xml_to_rrs,' +
+                            'example_json,example_xml}',
+                            '-t {excel,csv,json,xml,txt} -o OUTPUT',
+                            'extract_list cfg-example: error: the ' +
+                            'following arguments are required: ' +
+                            '-o/--output'])])
+def test_cmdline_nok1(capsys, line, errmsgs):
+    """Test not OK command lines 1."""
+    with pytest.raises(SystemExit):
+        _ = extract_cmd(arguments=line.split())
+    check_capsys(capsys=capsys, in_err=errmsgs)
