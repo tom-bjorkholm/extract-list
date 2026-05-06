d89ae741d4f350622bd5ead02eaa5ca0917430f9
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-05-06 15:04:56 +0200

    correct the arg filtering using new API

--- src/extract_list/handle_output.py
+++ src/extract_list/handle_output.py
@@ -13,8 +13,7 @@ from tableio import CsvDialect, FileAccess, OptionalArgsDict, \
 from extract_list.extract_config import ExtractConfig
 from extract_list.commontypes import Data
 from extract_list.config_enums import FormatRequest, \
-    get_outfile_capabilities, is_internal_out_file_format, \
-    list_out_format_implementations
+    get_outfile_capabilities, is_internal_out_file_format
 from extract_list.handle_json_xml_output import \
     handle_json_output, handle_xml_output
 
@@ -90,28 +89,17 @@ def _new_internal_filename(filename: str, extension: str) -> str:
     return fixed_fname
 
 
-def _get_out_tableio_impl(cfg: ExtractConfig) -> str:
-    """Get output implementation from configuration."""
-    if cfg.outfile_implementation is not None:
-        return cfg.outfile_implementation
-    # This must be corrected later
-    # For now we do not know highest piority implementation
-    # Waiting for expected TableIO API change.
-    return list_out_format_implementations(
-        format_name=cfg.outfile_type, border=cfg.outfile_border,
-        filtered_area=cfg.outfile_filtered_area)[0]
-
-
 def handle_tableio_output(data: Data, filename: str,
                           cfg: ExtractConfig) -> None:
     """Handle output through TableIO."""
     args = _tableio_optional_args(cfg)
-    out_impl = _get_out_tableio_impl(cfg)
     capabilities = get_outfile_capabilities(
         border=cfg.outfile_border, filtered_area=cfg.outfile_filtered_area)
-    filtered_args = filter_args_tableio(
-        args=args, format_name=cfg.outfile_type,
-        implementation=out_impl)
+    filtered_args = \
+        filter_args_tableio(args=args,
+                            format_name=cfg.outfile_type,
+                            implementation=cfg.outfile_implementation,
+                            capabilities=capabilities)
     with create_tableio(format_name=cfg.outfile_type, file_name=filename,
                         file_access=FileAccess.CREATE, args=filtered_args,
                         implementation=cfg.outfile_implementation,
